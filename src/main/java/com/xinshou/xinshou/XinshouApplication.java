7d7a85e4ca8d26d48d575309ac21fb6cfcb9756c
zhouguangzhi <z17610261959@163.com>
2019-09-01 20:21:20 +0800

    init repo

---
+++ src/main/java/com/xinshou/xinshou/XinshouApplication.java
@@ -0,0 +1,13 @@
+package com.xinshou.xinshou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinshouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinshouApplication.class, args);
+    }
+
+}
